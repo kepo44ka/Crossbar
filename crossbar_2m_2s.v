29957451e4fa0e867dd8f57b0fc17b2d1beef9c0
kepo44ka <taisia.sycheva@gmail.com>
2016-06-30 20:42:19 +0300

    Adding files

---
+++ crossbar_2m_2s.v
@@ -0,0 +1,227 @@
+module crossbar_2m_2s(
+  input wire master_1_req, master_2_req, 
+  input wire slave_1_ack, slave_2_ack,
+  input wire [31:0] master_1_addr, master_2_addr,
+  input wire [31:0] master_1_wdata, master_2_wdata,
+  input wire [31:0] slave_1_rdata, slave_2_rdata,
+  input wire master_1_cmd, master_2_cmd,
+  output reg slave_1_req, slave_2_req,
+  output reg master_1_ack, master_2_ack,
+  output reg [31:0] slave_1_addr, slave_2_addr,
+  output reg [31:0] slave_1_wdata, slave_2_wdata,
+  output reg [31:0] master_1_rdata, master_2_rdata,
+  output reg slave_1_cmd, slave_2_cmd
+);
+/*
+reg active_slave_1_req;
+reg active_slave_2_req;
+reg active_master_1_ack;
+reg active_master_2_ack;
+reg [31:0]active_slave_1_addr;
+reg [31:0]active_slave_2_addr;
+reg [31:0]active_slave_1_wdata;
+reg [31:0]active_slave_2_wdata;
+reg [31:0]active_master_1_rdata;
+reg [31:0]active_master_2_rdata;
+reg active_slave_1_cmd;
+reg active_slave_2_cmd;*/
+
+reg connect_req_m1_s1;
+reg connect_req_m2_s1;
+reg connect_req_m1_s2;
+reg connect_req_m2_s2;
+
+reg connect_approved_m1_s1;
+reg connect_approved_m2_s1;
+reg connect_approved_m1_s2;
+reg connect_approved_m2_s2;
+
+reg wdata_approved_m1_s1;
+reg wdata_approved_m2_s1;
+reg wdata_approved_m1_s2;
+reg wdata_approved_m2_s2;
+
+reg rdata_approved_m1_s1;
+reg rdata_approved_m2_s1;
+reg rdata_approved_m1_s2;
+reg rdata_approved_m2_s2;
+
+reg last_con_to_s1; //last connection to slave1 - wich master 1'b0 - 1st, 1'b1 - 2nd 
+reg last_con_to_s2; //last connection to slave2 - wich master 
+
+integer index;
+
+always@*
+begin
+connect_req_m1_s1 = (~master_1_addr[31]) & (master_1_req);
+connect_req_m2_s1 = (~master_2_addr[31]) & (master_2_req);
+connect_req_m1_s2 = (master_1_addr[31]) & (master_1_req);
+connect_req_m2_s2 = (master_2_addr[31]) & (master_2_req);
+
+//set last connection to slave 2 for both masters
+if (last_con_to_s1 | ~last_con_to_s1) 
+ begin
+ end
+else last_con_to_s1 = 1'b1;
+if (last_con_to_s2 | ~last_con_to_s2) 
+ begin
+ end
+else last_con_to_s2 = 1'b1;
+
+//resolving case if two M going to one slave
+if (master_1_req & master_2_req)
+ begin
+	//M1 M2 to separate s
+	if ((connect_req_m1_s1 & connect_req_m2_s2) | (connect_req_m1_s2 & connect_req_m2_s1))
+	    begin
+		connect_approved_m1_s1 = connect_req_m1_s1;
+		connect_approved_m1_s2 = connect_req_m1_s2;
+		connect_approved_m2_s1 = connect_req_m2_s1;
+		connect_approved_m2_s2 = connect_req_m2_s2;
+	    end
+
+	//M1 M2 to s1
+	if (connect_req_m1_s1 & connect_req_m2_s1)
+	     begin
+	     if (last_con_to_s1)
+	  	begin
+	    	//connect 1 master
+		connect_approved_m1_s1 = 1'b1;
+		connect_approved_m2_s1 = 1'b0;
+		last_con_to_s1 = 1'b0;
+	  	end
+	     else
+	  	begin
+	    	//connect 2 master
+		connect_approved_m2_s1 = 1'b1;
+		connect_approved_m1_s1 = 1'b0;
+		last_con_to_s1 = 1'b1;
+	  	end
+	     end
+	     
+
+	//M1 M2 to s2
+	if (connect_req_m1_s2 & connect_req_m2_s2)
+ 	  begin
+	     if (last_con_to_s2)
+	  	begin
+	    	//connect 1 master
+		connect_approved_m1_s2 = 1'b1;
+		connect_approved_m2_s2 = 1'b0;
+		last_con_to_s2 = 1'b0;
+	  	end
+	     else
+	  	begin
+	    	//connect 2 master
+		connect_approved_m2_s2 = 1'b1;
+		connect_approved_m1_s2 = 1'b0;
+		last_con_to_s2 = 1'b1;
+	  	end
+ 	  end
+
+
+ end
+else
+ begin
+	connect_approved_m1_s1 = connect_req_m1_s1;
+	connect_approved_m1_s2 = connect_req_m1_s2;
+	connect_approved_m2_s1 = connect_req_m2_s1;
+	connect_approved_m2_s2 = connect_req_m2_s2;
+ end
+
+wdata_approved_m1_s1 = (master_1_cmd) & (master_1_req) & (connect_approved_m1_s1);
+rdata_approved_m1_s1 = (~(master_1_cmd)) & (master_1_req) & (connect_approved_m1_s1);
+
+wdata_approved_m1_s2 = (master_1_cmd) & (master_1_req) & (connect_approved_m1_s2);
+rdata_approved_m1_s2 = (~(master_1_cmd)) & (master_1_req) & (connect_approved_m1_s2);
+
+wdata_approved_m2_s1 = (master_2_cmd) & (master_2_req) & (connect_approved_m2_s1);
+rdata_approved_m2_s1 = (~(master_2_cmd)) & (master_2_req) & (connect_approved_m2_s1);
+
+wdata_approved_m2_s2 = (master_2_cmd) & (master_2_req) & (connect_approved_m2_s2);
+rdata_approved_m2_s2 = (~(master_2_cmd)) & (master_2_req) & (connect_approved_m2_s2);
+
+
+//signals req, cmd, addr
+slave_1_req = connect_req_m1_s1 | connect_req_m2_s1;
+slave_2_req = connect_req_m1_s2 | connect_req_m2_s2;
+
+slave_1_cmd = ((master_1_cmd) & (connect_approved_m1_s1)) | ((master_2_cmd) & (connect_approved_m2_s1));
+slave_2_cmd = ((master_1_cmd) & (connect_approved_m1_s2)) | ((master_2_cmd) & (connect_approved_m2_s2));
+
+for(index = 0; index < 32; index = index + 1)
+  begin
+	slave_1_addr[index] = ((master_1_addr[index]) && (connect_approved_m1_s1)) || ((master_2_addr[index]) && (connect_approved_m2_s1));
+	slave_2_addr[index] = ((master_1_addr[index]) && (connect_approved_m1_s2)) || ((master_2_addr[index]) && (connect_approved_m2_s2));
+  end
+
+//last connections block
+if ((master_1_req) & (connect_approved_m1_s1))
+  begin
+	connect_approved_m1_s1 = 1'b0;
+	last_con_to_s1 = 1'b0;
+  end
+ if ((master_1_req) & (connect_approved_m1_s2))
+  begin
+	connect_approved_m1_s2 = 1'b0;
+	last_con_to_s2 = 1'b0;
+  end
+ if ((master_2_req) & (connect_approved_m2_s1))
+  begin
+	connect_approved_m2_s1 = 1'b0;
+	last_con_to_s1 = 1'b1;
+	  end
+ if ((master_2_req) & (connect_approved_m2_s2))
+  begin
+	connect_approved_m2_s2 = 1'b0;
+	last_con_to_s2 = 1'b1;
+  end
+
+end
+
+
+
+always @(slave_1_ack, slave_2_ack)
+begin
+
+	if ((slave_1_ack) & (master_1_addr[31]))
+	  begin
+	  end
+	else
+	  begin
+	  master_1_ack = slave_1_ack;
+	  end
+	if ((slave_1_ack) & (master_2_addr[31]))
+	  begin
+	  end
+	else
+	  begin
+	  master_2_ack = slave_1_ack;
+	  end
+
+
+	if ((slave_2_ack) & (master_1_addr[31]))
+	  begin
+	  master_1_ack = slave_2_ack;
+	  end
+
+	if ((slave_2_ack) & (master_2_addr[31]))
+	  begin
+	  master_2_ack = slave_2_ack;
+	  end
+end
+
+
+//data handling
+always @*
+begin
+for(index = 0; index < 32; index = index + 1)
+  begin
+slave_1_wdata[index] = (master_1_wdata[index] & wdata_approved_m1_s1) | (master_2_wdata[index] & wdata_approved_m2_s1);
+slave_2_wdata[index] = (master_1_wdata[index] & wdata_approved_m1_s2) | (master_2_wdata[index] & wdata_approved_m2_s2); 
+master_1_rdata[index] = (slave_1_rdata[index] & rdata_approved_m1_s1 & slave_1_ack) | (slave_2_rdata[index] & rdata_approved_m1_s2 & slave_2_ack);
+master_2_rdata[index] = (slave_1_rdata[index] & rdata_approved_m2_s1 & slave_1_ack) | (slave_2_rdata[index] & rdata_approved_m2_s2 & slave_2_ack);
+  end
+
+endmodule
+
---
+++ test_crossbar_2m_2s.v
@@ -0,0 +1,275 @@
+module testbench;
+
+  reg var_master_1_req;
+  reg var_master_2_req;
+  reg var_slave_1_ack;
+  reg var_slave_2_ack;
+  reg[31:0] var_master_1_addr;
+  reg[31:0] var_master_2_addr;
+  reg[31:0] var_master_1_wdata;
+  reg[31:0] var_master_2_wdata;
+  reg[31:0] var_slave_1_rdata;
+  reg[31:0] var_slave_2_rdata;
+  reg var_master_1_cmd;
+  reg var_master_2_cmd;
+
+wire out_slave_1_req; 
+wire out_slave_2_req;
+wire out_master_1_ack; 
+wire out_master_2_ack;
+wire out_slave_1_cmd; 
+wire out_slave_2_cmd;
+wire [31:0] out_master_1_rdata; 
+wire [31:0] out_master_2_rdata;
+wire [31:0] out_slave_1_wdata; 
+wire [31:0] out_slave_2_wdata;
+wire [31:0] out_slave_1_addr; 
+wire [31:0] out_slave_2_addr;
+
+//instance of module being studied
+crossbar_2m_2s crossbar(
+  .master_1_req(var_master_1_req),
+  .master_2_req(var_master_2_req),
+  .slave_1_ack(var_slave_1_ack),
+  .slave_2_ack(var_slave_2_ack),
+  .master_1_addr(var_master_1_addr),
+  .master_2_addr(var_master_2_addr),
+  .master_1_wdata(var_master_1_wdata),
+  .master_2_wdata(var_master_2_wdata),
+  .slave_1_rdata(var_slave_1_rdata),
+  .slave_2_rdata(var_slave_2_rdata),
+  .master_1_cmd(var_master_1_cmd),
+  .master_2_cmd(var_master_2_cmd),
+  .slave_1_req(out_slave_1_req),
+  .slave_2_req(out_slave_2_req),
+  .master_1_ack(out_master_1_ack),
+  .master_2_ack(out_master_2_ack),
+  .slave_1_addr(out_slave_1_addr),
+  .slave_2_addr(out_slave_2_addr),
+  .slave_1_wdata(out_slave_1_wdata),
+  .slave_2_wdata(out_slave_2_wdata),
+  .master_1_rdata(out_master_1_rdata),
+  .master_2_rdata(out_master_2_rdata),
+  .slave_1_cmd(out_slave_1_cmd),
+  .slave_2_cmd(out_slave_2_cmd)
+);
+
+initial
+begin
+
+var_master_1_req = 'b0;
+var_master_1_cmd = 'b0;
+var_slave_1_ack = 'b0;
+var_master_2_req = 'b0;
+var_master_2_cmd = 'b0;
+var_slave_2_ack = 'b0;
+ var_master_1_wdata = 'h1111;
+ var_master_2_wdata = 'h2222;
+ var_slave_1_rdata = 'h11;
+ var_slave_2_rdata = 'h22;
+ var_master_1_addr = 'hf0000000;
+ var_master_2_addr = 'hf0000000;
+
+
+
+
+
+
+
+
+//________WRITING TEST_________
+//Write to S1 from M1
+var_master_1_req = 'b1;
+var_master_1_cmd = 'b1;
+var_master_1_addr = 32'h7fffffff;
+var_master_1_wdata = 32'h11111111;
+#5;
+var_master_1_req = 'b0;
+var_slave_1_ack = 'b1;
+#5;
+var_slave_1_ack = 'b0;
+#15;
+
+//Write to S2 from M1
+var_master_1_req = 'b1;
+var_master_1_cmd = 'b1;
+var_master_1_addr = 32'hffffffff;
+var_master_1_wdata = 32'h22221111;
+#5;
+var_master_1_req = 'b0;
+var_slave_2_ack = 'b1;
+#5;
+var_slave_2_ack = 'b0;
+#15;
+
+//Write to S1 from M2
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b1;
+var_master_2_addr = 32'h7fffffff;
+var_master_2_wdata = 32'h11112222;
+#5;
+var_master_2_req = 'b0;
+var_slave_1_ack = 'b1;
+#5;
+var_slave_1_ack = 'b0;
+#15;
+
+//Write to S2 from M2
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b1;
+var_master_2_addr = 32'hffffffff;
+var_master_2_wdata = 32'h22222222;
+#5;
+var_master_2_req = 'b0;
+var_slave_2_ack = 'b1;
+#5;
+var_slave_2_ack = 'b0;
+#15;
+
+//________READING TEST_________
+//Read from S1 to M1
+var_master_1_req = 'b1;
+var_master_1_cmd = 'b0;
+var_master_1_addr = 'h7fffffff;
+var_slave_1_rdata = 'h10000001;
+#5;
+var_slave_1_ack = 'b1;
+#5;
+var_master_1_req = 'b0;
+var_slave_1_ack = 'b0;
+#15;
+
+//Read from S2 to M1
+var_master_1_req = 'b1;
+var_master_1_cmd = 'b0;
+var_master_1_addr = 'hffffffff;
+var_slave_2_rdata = 'h20000001;
+#5;
+var_slave_1_ack = 'b1;
+#5;
+var_master_1_req = 'b0;
+var_slave_1_ack = 'b0;
+#15;
+
+//Read from S1 to M2
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b0;
+var_master_2_addr = 'h7fffffff;
+var_slave_1_rdata = 'h10000002;
+#5;
+var_slave_1_ack = 'b1;
+#5;
+var_master_2_req = 'b0;
+var_slave_1_ack = 'b0;
+#15;
+
+//Read from S2 to M2
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b0;
+var_master_2_addr = 'hffffffff;
+var_slave_2_rdata = 'h20000002;
+#5;
+var_slave_2_ack = 'b1;
+#5;
+var_master_2_req = 'b0;
+var_slave_2_ack = 'b0;
+#15;
+
+//________2 Request situations_________
+//Write to different slaves 200ns
+var_master_1_req = 'b1;
+var_master_1_cmd = 'b1;
+var_master_1_addr = 'h7fffffff;
+var_master_1_wdata = 'h11111111;
+
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b1;
+var_master_2_addr = 32'hffffffff;
+var_master_2_wdata = 32'h22221111;
+#5;
+var_master_1_req = 'b0;
+var_slave_1_ack = 'b1;
+var_master_2_req = 'b0;
+var_slave_2_ack = 'b1;
+#5;
+var_slave_1_ack = 'b0;
+var_slave_2_ack = 'b0;
+#15;
+
+//Read from different slaves 225ns
+var_master_1_req = 'b1;
+var_master_1_cmd = 'b0;
+var_master_1_addr = 'h7fffffff;
+var_slave_1_rdata = 'h10000001;
+
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b0;
+var_master_2_addr = 'hffffffff;
+var_slave_2_rdata = 'h20000002;
+#5;
+var_slave_1_ack = 'b1;
+var_slave_2_ack = 'b1;
+#5;
+var_master_1_req = 'b0;
+var_slave_1_ack = 'b0;
+var_master_2_req = 'b0;
+var_slave_2_ack = 'b0;
+#15;
+
+//Write to the same slave (1st) 2 times  250ns
+var_master_1_req = 'b1;			//1st
+var_master_1_cmd = 'b1;
+var_master_1_addr = 'h7fffffff;
+var_master_1_wdata = 'h11111111;
+
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b1;
+var_master_2_addr = 32'h1fffffff;
+var_master_2_wdata = 32'h11112222;
+#5;
+var_master_1_req = 'b0;
+var_master_2_req = 'b0;
+var_slave_1_ack = 'b1;
+#5;
+var_slave_1_ack = 'b0;
+#15;
+
+var_master_1_req = 'b1;			//2nd
+var_master_1_cmd = 'b1;
+var_master_1_addr = 'h7fffffff;
+var_master_1_wdata = 'h11111111;
+
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b1;
+var_master_2_addr = 32'h1fffffff;
+var_master_2_wdata = 32'h11112222;
+#5;
+var_master_1_req = 'b0;
+var_master_2_req = 'b0;
+var_slave_1_ack = 'b1;
+#5;
+var_slave_1_ack = 'b0;
+#15;
+
+//Read from the same slave  300 ns
+var_master_1_req = 'b1;
+var_master_1_cmd = 'b0;
+var_master_1_addr = 'h7fffffff;
+var_slave_1_rdata = 'h10000001;
+
+var_master_2_req = 'b1;
+var_master_2_cmd = 'b0;
+var_master_2_addr = 'hffffffff;
+var_slave_2_rdata = 'h20000002;
+#5;
+var_slave_1_ack = 'b1;
+#5;
+var_master_1_req = 'b0;
+var_slave_1_ack = 'b0;
+var_master_2_req = 'b0;
+#15;
+
+end
+
+
+endmodule
