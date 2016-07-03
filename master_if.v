7035a0af4d11d1fde40e088a0088a257a30eec40
kepo44ka <taisia.sycheva@gmail.com>
2016-07-03 12:02:32 +0300

    works in modelsim for 25ps

--- master_if.v
+++ master_if.v
@@ -9,19 +9,20 @@ module master_if(
   
   input wire connect_approved_from_crossbar,
   
-  output reg ack_to_master,
-  output reg [31:0] rdata_to_master,
+  output wire ack_to_master,
+  output [31:0] rdata_to_master,
   
   output reg req_to_crossbar, 
   output reg [31:0] addr_to_crossbar,
   output reg [31:0] wdata_to_crossbar,
   output reg cmd_to_crossbar
 );
+
+assign ack_to_master	= ack_from_crossbar & connect_approved_from_crossbar;
+assign rdata_to_master	= rdata_from_crossbar & {32{ack_from_crossbar}} & {32{connect_approved_from_crossbar}};
+  
 always @*
 begin
-  ack_to_master		= ack_from_crossbar & connect_approved_from_crossbar;
-  rdata_to_master	= rdata_from_crossbar & {32{ack_from_crossbar}} & {32{connect_approved_from_crossbar}};
-  
   req_to_crossbar	= req_from_master; 
   addr_to_crossbar	= addr_from_master & {32{connect_approved_from_crossbar}};
   wdata_to_crossbar	= wdata_from_master & {32{connect_approved_from_crossbar}};
@@ -29,3 +30,4 @@ begin
 end
 
 endmodule
+
--- slave_if.v
+++ slave_if.v
@@ -13,21 +13,21 @@ module slave_if(
   output reg ack_to_crossbar,    
   output reg [31:0] rdata_to_crossbar,
   
-  output reg req_to_slave,
-  output reg [31:0] addr_to_slave,
-  output reg [31:0] wdata_to_slave,
-  output reg cmd_to_slave
+  output req_to_slave,
+  output [31:0] addr_to_slave,
+  output [31:0] wdata_to_slave,
+  output cmd_to_slave
 );
+
+assign req_to_slave	= req_from_crossbar;
+assign addr_to_slave	= addr_from_crossbar & {32{connect_approved_from_crossbar}};
+assign wdata_to_slave	= wdata_from_crossbar & {32{cmd_from_crossbar}} & {32{connect_approved_from_crossbar}}; //{32{cmd_from_crossbar}} <= write command
+assign cmd_to_slave	= cmd_from_crossbar & connect_approved_from_crossbar;
 
 always @*
 begin
   ack_to_crossbar	= ack_from_slave;
   rdata_to_crossbar	= rdata_from_slave & ~{32{cmd_from_crossbar}} & {32{connect_approved_from_crossbar}}; //~{32{cmd_from_crossbar}} <= read command
-  
-  req_to_slave		= req_from_crossbar; 
-  addr_to_slave		= addr_from_crossbar & {32{connect_approved_from_crossbar}};
-  wdata_to_slave	= wdata_from_crossbar & {32{cmd_from_crossbar}} & {32{connect_approved_from_crossbar}}; //{32{cmd_from_crossbar}} <= write command
-  cmd_to_slave		= cmd_from_crossbar & connect_approved_from_crossbar;
 end
 
 endmodule
